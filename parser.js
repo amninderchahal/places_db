275f39a1ada0a931de86c086e377c32e48d66826
Amninder Chahal <amninderchahal@Amninders-MacBook-Pro.local>
2020-12-23 16:43:52 -0500

    replace country object id with country code for file names

--- parser.js
+++ parser.js
@@ -6,9 +6,13 @@ const countriesDir = 'places/';
 const citiesDir = 'places/cities/';
 const subdivisionsDir = 'places/subdivisions/';
 
+// Load raw data
+let countriesRawData = fs.readFileSync('Country.json');
+let countries = JSON.parse(countriesRawData).results;
+let statesRawData = fs.readFileSync('Subdivisions_States_Provinces.json');
+let states = JSON.parse(statesRawData).results;
 let citiesRawdata = fs.readFileSync('City.json');
 let cities = JSON.parse(citiesRawdata).results;
-let parsedCities = {};
 
 // Create empty directories if they don't exist
 if (!fs.existsSync(countriesDir)) {
@@ -21,14 +25,33 @@ if (!fs.existsSync(subdivisionsDir)) {
   fs.mkdirSync(subdivisionsDir);
 }
 
+
+// Process Countries
+const countriesMap = {};
+const parsedCountries = countries.map(country => {
+  countriesMap[country.objectId] = country;
+  return {
+    name: country.name,
+    code: country.code
+  };
+});
+
+fs.writeFileSync(countriesDir + 'countries.json', JSON.stringify(parsedCountries, null, 4));
+
+console.log('Parsing countries complete');
+
+// Process cities
+let parsedCities = {};
+
 for (let city of cities) {
-  let parsedCity = parsedCities[city.country.objectId];
+  const country = countriesMap[city.country.objectId];
+  let parsedCity = parsedCities[country.code];
   if (parsedCity == null) {
-    parsedCities[city.country.objectId] = {
+    parsedCities[country.code] = {
       items: [],
       addedKeys: {}
     };
-    parsedCity = parsedCities[city.country.objectId];
+    parsedCity = parsedCities[country.code];
   }
   if (!parsedCity.addedKeys[city.name]) {
     parsedCity.items.push(city.name);
@@ -36,54 +59,38 @@ for (let city of cities) {
   }
 }
 
-Object.entries(parsedCities).forEach(([id, { items }]) => {
+Object.entries(parsedCities).forEach(([code, { items }]) => {
   let data = JSON.stringify(items, null, 4);
-  fs.writeFileSync(citiesDir + id + '.json', data);
+  fs.writeFileSync(citiesDir + code + '.json', data);
 });
 
 console.log('Parsing cities complete');
 
-// States
-
-let statesRawData = fs.readFileSync('Subdivisions_States_Provinces.json');
-let states = JSON.parse(statesRawData).results;
+// Process States
 let parsedStates = {};
 
 for (let state of states) {
-  let parsedState = parsedStates[state.country.objectId];
+  const country = countriesMap[state.country.objectId];
+  let parsedState = parsedStates[country.code];
   if (parsedState == null) {
-    parsedStates[state.country.objectId] = {
+    parsedStates[country.code] = {
       items: [],
       addedKeys: {}
     };
-    parsedState = parsedStates[state.country.objectId];
+    parsedState = parsedStates[country.code];
   }
-  if (!parsedState.addedKeys[state.Subdivision_Name]) {
+  if (!parsedState.addedKeys[state.Subdivision_Code]) {
     parsedState.items.push({
       name: state.Subdivision_Name,
       code: state.Subdivision_Code
     });
-    parsedState.addedKeys[state.Subdivision_Name] = true;
+    parsedState.addedKeys[state.Subdivision_Code] = true;
   }
 }
 
-Object.entries(parsedStates).forEach(([id, { items }]) => {
+Object.entries(parsedStates).forEach(([code, { items }]) => {
   let data = JSON.stringify(items, null, 4);
-  fs.writeFileSync(subdivisionsDir + id + '.json', data);
+  fs.writeFileSync(subdivisionsDir + code + '.json', data);
 });
 
 console.log('Parsing subdivisions complete');
-
-// Countries
-
-let countriesRawData = fs.readFileSync('Country.json');
-let countries = JSON.parse(countriesRawData).results;
-let parsedCountries = countries.map(country => ({
-  id: country.objectId,
-  name: country.name,
-  code: country.code
-}));
-
-fs.writeFileSync(countriesDir + 'countries.json', JSON.stringify(parsedCountries, null, 4));
-
-console.log('Parsing countries complete');
